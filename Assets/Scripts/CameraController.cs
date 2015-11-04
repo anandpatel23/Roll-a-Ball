2be60e06c2a6429d3a374f4e4a4dd67ab87baf7c
anandpatel23 <ap947@drexel.edu>
2015-11-04 15:29:09 -0500

    Adding all

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        // camera is moved every frame aligned with the player object
+        transform.position = player.transform.position + offset;
+	}
+}
